e30b173aa01321bd7cc53ad02c8cb10bfbe86af7
gengplannonz <geng.plan.n.onz@gmail.com>
2026-07-24 18:28:36 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,1 @@
-const API = "https://script.google.com/macros/s/AKfycbz_CSK_X-pPmk475dFem3RSjWnuB3_OccpLpn2pM6WCjs32_XFCBU3OVGsa5afhIFKL/exec";
+const API = "https://script.google.com/macros/s/AKfycbz_CSK_X-pPmk475dFem3RSjWnuB3_OccpLpn2pM6WCjs32_XFCBU3OVGsa5afhIFKL/exec?action=getMembers";
